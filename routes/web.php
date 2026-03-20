df9dc469a819d97ad18057f93dcfe8c9f25c9732
Kamil Kozłowski <giogurto.grande@outlook.com>
2026-03-20 18:39:30 +0100

    login & logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
@@ -14,9 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 });
 
-Route::view('/register', 'auth.register')
-    ->middleware('guest')
-    ->name('register');
+Route::view('/register', 'auth.register')->middleware('guest')->name('register');
+Route::post('/register', Register::class)->middleware('guest');
 
-Route::post('/register', Register::class)
-    ->middleware('guest');
+Route::view('/login', 'auth.login')->middleware('guest')->name('login');
+Route::post('/login', Login::class)->middleware('guest');
+
+Route::post('/logout', Logout::class)->middleware('auth')->name('logout');
